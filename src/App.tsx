8805be0fd4cc501f4beda3b48b9b93e146b1dd4d
Provaldo <manos.sybo@gmail.com>
2022-07-18 20:05:00 +0300

    Completed the simple react app with typescript from TheUltimateTypescriptCourse by Mosh Hamedani

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+import reminderService from './services/reminder';
+import NewReminder from './components/NewReminder';
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  }
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter(reminder => reminder.id !== id));
+  }
+
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <button className="btn btn-primary">Click me</button> */}
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder}/>
+
+
+      {/* ############# ORIGINAL CONTENT #################
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +48,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
